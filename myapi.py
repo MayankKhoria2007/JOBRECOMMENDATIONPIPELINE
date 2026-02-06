588212b21006622bfb208c43282e16183c24e664
MayankKhoria2007 <mayank.khoria2007@gmail.com>
2026-02-07 00:11:36 +0530

    Add files via upload

---
+++ myapi.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI,WebSocket,WebSocketDisconnect
+from typing import Optional,List
+from redis_storage import redis_storage
+from contentbasedrecommendation import content_based_recommendation
+
+app=FastAPI()
+
+
+
+@app.post("/store-resume-embedding")
+def create(user_id:str,parsed_resume:dict):
+    ob=redis_storage()
+    ob.concat_resume(parsed_resume)
+    ob.embedding_generator()
+    ob.store_embedding(user_id)
+    return {"message":"resume embeddings stored successfully in redis"}
+
+
+
+
+@app.websocket("/ws/get-job-recommendations-for-user/{user_id}")
+async def websocket_fetch(websocket:WebSocket,user_id:str):
+    await websocket.accept()
+
+
+    try:
+        while  True:
+            job_descriptions=await websocket.receive_json()
+            ob=content_based_recommendation(len(job_descriptions))
+            ob.concatenate_job_fields(job_descriptions)
+            ob.recommendation(user_id)
+            await websocket.send_json(ob.job_recommendations)
+
+    except WebSocketDisconnect:
+        print("Client disconnected")
+
